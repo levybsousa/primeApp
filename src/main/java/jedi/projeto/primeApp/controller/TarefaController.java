009cde6ce2301d1419cfefaebca9aaf1b966ce81
levybsousa <levybsousa02@gmail.com>
2023-08-03 11:33:16 -0300

    Conectando TarefaController com TarefaService

--- src/main/java/jedi/projeto/primeApp/controller/TarefaController.java
+++ src/main/java/jedi/projeto/primeApp/controller/TarefaController.java
@@ -13,26 +13,30 @@ import java.util.List;
 public class TarefaController {
 
     private final TarefaService tarefaService;
+
     @GetMapping
-    public List<Tarefa> getAll(){
-        return null;
+    public List<Tarefa> getAll() {
+        return tarefaService.getAll();
     }
 
     @GetMapping("/{id}")
-    public Tarefa getById(@PathVariable Long id){
-        return null;
+    public Tarefa getById(@PathVariable Long id) {
+        return tarefaService.getById(id);
     }
-    @PostMapping
-    public Tarefa save(@RequestBody Tarefa tarefa ){
-        return null;
-    }
-    @PutMapping("/{id}")
-    public Tarefa update(@PathVariable Long id , @RequestBody Tarefa tarefa){
-        return null;
-    }
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
 
+    @PostMapping
+    public Tarefa save(@RequestBody Tarefa tarefa) {
+        return tarefaService.save(tarefa);
+    }
+
+    @PutMapping("/{id}")
+    public Tarefa update(@PathVariable Long id, @RequestBody Tarefa tarefa) {
+        return tarefaService.update(id, tarefa);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        tarefaService.delete(id);
     }
 
 }
