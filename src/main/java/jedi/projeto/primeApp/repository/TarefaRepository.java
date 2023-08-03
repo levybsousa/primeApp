4144b5abc14894f1a78305693087da3260f8e7d7
levybsousa <levybsousa02@gmail.com>
2023-08-03 11:29:52 -0300

    Adicionando dependências

--- src/main/java/jedi/projeto/primeApp/repository/TarefaRepository.java
+++ src/main/java/jedi/projeto/primeApp/repository/TarefaRepository.java
@@ -1,4 +1,13 @@
 package jedi.projeto.primeApp.repository;
 
-public interface TarefaRepository {
+import jedi.projeto.primeApp.model.entity.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    Optional<Tarefa> getTarefaByNome(String nome);
 }
--- src/main/java/jedi/projeto/primeApp/service/TarefaService.java
+++ src/main/java/jedi/projeto/primeApp/service/TarefaService.java
@@ -1,6 +1,7 @@
 package jedi.projeto.primeApp.service;
 
 import jedi.projeto.primeApp.model.entity.Tarefa;
+import jedi.projeto.primeApp.repository.TarefaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,8 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TarefaService {
+    
+    private final TarefaRepository tarefaRepository;
 
     public List<Tarefa> getAll() {
         return null;
@@ -17,13 +20,16 @@ public class TarefaService {
     public Tarefa getById(Long id) {
         return null;
     }
-    public Tarefa save(Tarefa tarefa){
+
+    public Tarefa save(Tarefa tarefa) {
         return null;
     }
-    public Tarefa update(Long id , Tarefa tarefa){
+
+    public Tarefa update(Long id, Tarefa tarefa) {
         return null;
     }
-    public void delete(Long id){
+
+    public void delete(Long id) {
 
     }
 }
